d0d0a4a1b2cb1991ae3b93680926003b27daf1bc
John Dang <john.amq.dang@gmail.com>
2019-06-19 18:34:11 -0700

    progress on website changes including layout and typed js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#typed", {
+    strings: ["a computer science student. ^1000", "an explorer. ^1000", "a problem solver. ^1000",
+        "a quick learner. ^1000", "a machine learning researcher / engineer. ^1000",
+        "a software deverloper. ^1000", "a builder. ^1000"
+    ],
+    loop: true,
+    showCursor: true,
+    typeSpeed: 25
+});

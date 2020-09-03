652b044ff9983efeccb79cc256eaa9bf99d3ba7a
John Dang <john.amq.dang@gmail.com>
2020-09-03 01:15:48 -0700

    upadate site

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-var typed = new Typed("#typed", {
-    strings: [ "^1000", "computer science student. ^1000", "machine learning researcher / engineer. ^1000",
-                "explorer. ^1000", "problem solver. ^1000", "quick learner. ^1000", "software deverloper. ^1000"
-    ],
-    loop: true,
-    showCursor: true,
-    typeSpeed: 25
-});
+var typed = 0
 
 
 
